9867ab0121bd0bc0d9f2c834256780045e60be46
akshayrlx <akshayrlx4@gmail.com>
2025-08-12 14:34:58 +0530

    Add files via upload

---
+++ basics_of_R.R
@@ -0,0 +1,69 @@
+a<-10
+a
+
+name <- "johny"
+
+list<- list(1,2,3,4)
+date<-date()
+date
+month<- month.name
+month
+montn1<-month.abb
+montn1
+
+details<- list(name="akshay",age=21,place="ottapalam")
+details$name
+details[2]
+vector<- c(1,2,3,4)
+vector
+
+b=20
+c=9.0
+typeof(b)
+typeof(c)
+
+class(c)
+
+d=TRUE
+
+class(d)
+
+e=b<c
+class(e)
+
+f<- "frog"
+class(f)
+
+typeof(b)
+
+
+num_vector <- c(1,2,3,4)
+class(num_vector)
+
+char_vector<- c("apple","banana")
+char_vector
+class(char_vector)
+
+log_vector<- c(TRUE,FALSE)
+class(log_vector)
+
+seq_vector<- 1:10
+seq_vector
+
+seq_vector2<- seq(from=1,to=20,by=2)
+seq_vector2
+
+rep_vector<- rep(10, times=10)
+rep_vector
+
+mixed_vec <- c(1, "R", TRUE)
+print(mixed_vec)
+class(mixed_vec)
+
+rep_seq<-rep(c(1,2,3),times=3)
+rep_seq
+
+num_vector[2:4]
+
+
+
